f5432cc9ff7c5e3c092cee82d6607cc0eaba07d7
minsoo-web <zlemzlem5656@naver.com>
2024-05-18 06:53:44 +0900

    feat: floating and framer-motion

--- apps/web/app/_shared/components/People/People.tsx
+++ apps/web/app/_shared/components/People/People.tsx
@@ -1,7 +1,23 @@
+'use client'
+
+import { FloatingArrow, arrow, useFloating } from '@floating-ui/react'
 import { Avatar, AvatarFallback, AvatarImage } from '@ui/components'
+import { AnimatePresence, motion } from 'framer-motion'
+import { useRef } from 'react'
+import { useTimeout } from 'react-use'
 import { 클라이머 } from './People.constants'
 
 export const People = () => {
+  const arrowRef = useRef(null)
+  const { refs, floatingStyles, context } = useFloating({
+    middleware: [
+      arrow({
+        element: arrowRef,
+      }),
+    ],
+  })
+  const [isReady] = useTimeout(500)
+
   return (
     <div className="mb-3 py-3 border-top">
       <a
@@ -13,7 +29,7 @@ export const People = () => {
         People
       </a>
 
-      <div className="flex flex-wrap mt-3 gap-1">
+      <div ref={refs.setReference} className="flex flex-wrap mt-3 gap-1">
         {클라이머.map(({ name, username }) => (
           <a href={`https://github.com/${username}`} target="_blank" rel="noreferrer" key={name}>
             <Avatar>
@@ -23,6 +39,38 @@ export const People = () => {
           </a>
         ))}
       </div>
+
+      <AnimatePresence>
+        {isReady() && (
+          <motion.div
+            ref={refs.setFloating}
+            style={floatingStyles}
+            className="relative bg-black text-white p-4"
+            initial={{ top: 0, opacity: 0 }}
+            variants={{
+              spring: {
+                top: 30,
+                transition: {
+                  type: 'spring',
+                  damping: 3,
+                  // Stiffness of the spring. Higher values will create more sudden movement. Set to 100 by default.
+                  stiffness: 200,
+                },
+              },
+              fadeIn: {
+                opacity: 1,
+                transition: {
+                  duration: 0.5,
+                },
+              },
+            }}
+            animate={['spring', 'fadeIn']}
+          >
+            show more information
+            <FloatingArrow ref={arrowRef} context={context} />
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   )
 }
