371ca89d16a036fc995eb4787d34fb25778eb0ca
Minsoo Kim <57122180+minsoo-web@users.noreply.github.com>
2024-05-19 01:30:32 +0900

    tailwind, shadcn ui 중복 설정 개선 #14 (#16)
    * tailwind, shadcn ui 중복 설정 개선 #14
    
    * chore: 충돌간 해결

--- apps/home/app/layout.tsx
+++ apps/home/app/layout.tsx
@@ -1,4 +1,4 @@
-import './globals.css'
+import '@hamsurang/ui/globals.css'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
---
+++ web/tailwind.config.ts
@@ -0,0 +1,1 @@
+export * from '@hamsurang/ui/tailwind.config'
