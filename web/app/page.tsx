0175944406dc837e204127595792d5eb040f068f
minsoo-web <zlemzlem5656@naver.com>
2024-05-18 09:09:13 +0900

    chore: production URL 수정

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,7 +1,5 @@
 const URL =
-  process.env.NODE_ENV === 'development'
-    ? 'http://localhost:3002'
-    : 'https://activity.hamsurang.com'
+  process.env.NODE_ENV === 'development' ? 'http://localhost:3002' : 'https://home.hamsurang.com'
 
 export default function Page() {
   return <iframe src={URL} style={{ width: '100%', height: '100vh' }} title="home" />
