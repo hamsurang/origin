e3af0ea3b126c600e7cc8560b2eb4aa81be5b258
minsoo-web <zlemzlem5656@naver.com>
2024-05-19 01:25:07 +0900

    chore: 충돌간 해결

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -1,4 +1,4 @@
-import './globals.css'
+import '@hamsurang/ui/globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { GlobalNavigationBar } from './_shared/components/GlobalNavigationBar'
