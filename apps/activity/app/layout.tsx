4aab42d7852b6c397f747475559149b318ab02e2
Chung-il Jung <jungchungil7@gmail.com>
2024-06-09 00:21:27 +0900

    feat: SideNav 컴포넌트

---
+++ apps/activity/app/layout.tsx
@@ -0,0 +1,40 @@
+import '@hamsurang/ui/globals.css'
+
+import { Inter } from 'next/font/google'
+import { SideNav } from './_shared'
+
+const inter = Inter({ subsets: ['latin'] })
+
+const NAV_ITEMS = [
+  {
+    id: 1,
+    title: '정규활동',
+    subItems: [
+      { id: 1, name: '함수랑마라톤', url: '/hamsurang/origin/wiki/함수랑마라톤' },
+      { id: 2, name: '함수랑학예회', url: '/hamsurang/origin/wiki/함수랑학예회' },
+    ],
+  },
+  {
+    id: 2,
+    title: '디스코드',
+    subItems: [
+      { id: 3, name: '함수랑크리틱', url: '/hamsurang/origin/wiki/함수랑크리틱' },
+      { id: 4, name: '함수랑상영회', url: '/hamsurang/origin/wiki/함수랑상영회' },
+    ],
+  },
+]
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}): JSX.Element {
+  return (
+    <html lang="ko">
+      <body className={inter.className}>
+        {children}
+        <SideNav items={NAV_ITEMS} />
+      </body>
+    </html>
+  )
+}
