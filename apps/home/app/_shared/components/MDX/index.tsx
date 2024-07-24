8984397aff15458adcad16659a571e4fdef37cf5
sonsurim <surim014@naver.com>
2024-07-24 21:02:57 +0900

    fix a wrong path

--- apps/home/app/_shared/components/MDX/index.tsx
+++ apps/home/app/_shared/components/MDX/index.tsx
@@ -1,5 +1,5 @@
 import { MDXRemote } from 'next-mdx-remote/rsc'
-import { MDXStylingComponents } from './MDX.style'
+import { MDXStylingComponents } from './MDX.styles'
 
 export const MDX = ({ fileText }: { fileText: string }) => (
   <MDXRemote source={fileText} components={MDXStylingComponents} />
