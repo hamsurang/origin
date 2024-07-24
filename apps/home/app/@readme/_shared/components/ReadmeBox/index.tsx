89cba7904ae999f2a577220ffa48255a3dd1da99
Sonny <47546413+sonsurim@users.noreply.github.com>
2024-07-24 19:52:59 +0900

    Update apps/home/app/@readme/_shared/components/ReadmeBox/index.tsx
    Co-authored-by: Minsoo Kim <57122180+minsoo-web@users.noreply.github.com>

--- apps/home/app/@readme/_shared/components/ReadmeBox/index.tsx
+++ apps/home/app/@readme/_shared/components/ReadmeBox/index.tsx
@@ -1,5 +1,5 @@
 import type { PropsWithChildren } from 'react'
 
 export const ReadmeBox = ({ children }: PropsWithChildren) => (
-  <div className="border border-[#d0d7de] rounded-[6px] p-[24px]">{children}</div>
+  <div className="border-slate-200 border-2 rounded-lg p-[24px]">{children}</div>
 )
