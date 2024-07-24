a95951f1b30e854094ad00bcc0861ee353aa7be2
sonsurim <surim014@naver.com>
2024-07-24 20:59:47 +0900

    force movement of unrecognized files

--- apps/home/app/_shared/components/Mdx/index.tsx
+++
@@ -1,6 +0,0 @@
-import { MDXRemote } from 'next-mdx-remote/rsc'
-import { MDXStylingComponents } from './MDX.style'
-
-export const MDX = ({ fileText }: { fileText: string }) => (
-  <MDXRemote source={fileText} components={MDXStylingComponents} />
-)
