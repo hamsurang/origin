6a64e1636ce959c3adabb3cabaf3a75ba64584a1
minsoo-web <zlemzlem5656@naver.com>
2024-08-01 18:59:20 +0900

    hotfix: vercel 에서 searchParam 못 읽는 문제 임시방편

--- apps/home/app/@profile/people/page.tsx
+++ apps/home/app/@profile/people/page.tsx
@@ -1,15 +1,15 @@
+'use client'
+
+import { useSearchParams } from 'next/navigation'
 import { Profile } from '../../_shared'
 import { PROFILE_INFO } from './people.constants'
 
-export default function Page({
-  searchParams,
-}: {
-  searchParams: { username: string }
-}) {
+export default function Page() {
+  const { get } = useSearchParams()
   /**
    * middleware에서 username을 검증하므로 타입 단언을 사용합니다.
    */
-  const userInfo = PROFILE_INFO[searchParams.username as keyof typeof PROFILE_INFO]
+  const userInfo = PROFILE_INFO[get('username') as keyof typeof PROFILE_INFO]
 
   return <Profile {...userInfo} />
 }
