a613e5d32273c3a128aa9ef328d5efc60f1c057a
sonsurim <surim014@naver.com>
2024-08-07 00:18:42 +0900

    using outputFileTracingIncludes

--- apps/home/app/@readme/default.tsx
+++ apps/home/app/@readme/default.tsx
@@ -3,7 +3,7 @@ import path from 'node:path'
 import { Readme } from '@/_shared'
 
 export default async function Page() {
-  const filePath = path.resolve(process.cwd(), 'public', 'content', 'main.mdx')
+  const filePath = path.join(process.cwd(), './contents/main.mdx')
   const mainReadme = await fs.readFile(filePath, { encoding: 'utf8' })
 
   return <Readme text={mainReadme} />
