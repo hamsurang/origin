b550c0e19f7cfee5024ae5d78c525420374f1a37
Sonny <surim014@naver.com>
2024-09-09 23:49:20 +0900

    chore: barrel file 분리 (#38)

--- apps/home/app/_shared/components/Readme/index.tsx
+++ apps/home/app/_shared/components/Readme/index.tsx
@@ -1,8 +1,1 @@
-import { MDXRemote } from 'next-mdx-remote/rsc'
-import { MDXStylingComponents } from './MDX.styles'
-
-export const Readme = ({ text }: { text: string }) => (
-  <div className="border-slate-200 border-2 rounded-lg p-[24px]">
-    <MDXRemote source={text} components={MDXStylingComponents} />
-  </div>
-)
+export * from './Readme'
