db7f87d4413cbf149a066a69b2d9f29e4e2b52c5
sonsurim <surim014@naver.com>
2024-09-14 05:27:19 +0900

    wiki 페이지 mdx 초기 설정

--- apps/wiki/next.config.js
+++ apps/wiki/next.config.js
@@ -1,4 +1,11 @@
+const withMDX = require('@next/mdx')()
+
 /** @type {import('next').NextConfig} */
-module.exports = {
+const nextConfig = {
+  pageExtensions: ['ts', 'tsx', 'mdx'],
   transpilePackages: ['@hamsurang/ui'],
+  experimental: {
+    typedRoutes: true,
+  },
 }
+module.exports = withMDX(nextConfig)
