86c56143307a35e40d618357ec52d01f0053e957
sonsurim <surim014@naver.com>
2024-09-14 17:02:26 +0900

    템플릿리터럴을 cn으로 대체 리뷰 반영

--- apps/wiki/app/_shared/components/Readme/Readme.tsx
+++ apps/wiki/app/_shared/components/Readme/Readme.tsx
@@ -1,8 +1,9 @@
+import { cn } from '@hamsurang/ui'
 import { MDXRemote } from 'next-mdx-remote/rsc'
 import { MDXStylingComponents } from './MDX.styles'
 
 export const Readme = ({ text, className }: { text: string; className?: string }) => (
-  <div className={`border-slate-200 border-2 rounded-lg p-[24px] ${className}`}>
+  <div className={cn('border-slate-200 border-2 rounded-lg p-[24px]', className)}>
     <MDXRemote source={text} components={MDXStylingComponents} />
   </div>
 )
