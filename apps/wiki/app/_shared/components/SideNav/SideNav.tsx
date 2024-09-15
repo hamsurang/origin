4e69d320d7a27887acb8f689fac24188d38a0513
minsoo-web <zlemzlem5656@naver.com>
2024-09-15 20:22:21 +0900

    hotfix: 불필요한 button 태그 삭제 & 기본이 열려있는 상태로 변경

--- apps/wiki/app/_shared/components/SideNav/SideNav.tsx
+++ apps/wiki/app/_shared/components/SideNav/SideNav.tsx
@@ -28,19 +28,17 @@ export const SideNav = ({ items }: SideNavProps) => {
         <ul className="p-0 list-none">
           {items.map((item) => (
             <li key={item.id} className="border-b">
-              <details className="group">
+              <details className="group" open>
                 <summary className="cursor-pointer flex items-center p-2">
-                  <button type="button" className="p-1 bg-transparent transition-transform">
-                    <svg
-                      aria-hidden="true"
-                      height="16"
-                      viewBox="0 0 16 16"
-                      width="16"
-                      className="group-open:rotate-0 -rotate-90"
-                    >
-                      <path d="m4.427 7.427 3.396 3.396a.25.25 0 0 0 .354 0l3.396-3.396A.25.25 0 0 0 11.396 7H4.604a.25.25 0 0 0-.177.427Z" />
-                    </svg>
-                  </button>
+                  <svg
+                    aria-hidden="true"
+                    height="16"
+                    viewBox="0 0 16 16"
+                    width="16"
+                    className="group-open:rotate-0 -rotate-90"
+                  >
+                    <path d="m4.427 7.427 3.396 3.396a.25.25 0 0 0 .354 0l3.396-3.396A.25.25 0 0 0 11.396 7H4.604a.25.25 0 0 0-.177.427Z" />
+                  </svg>
                   <span className="ml-2 text-sm font-bold text-blue-500 truncate">
                     <Link
                       href={`?id=${item.id}`}
