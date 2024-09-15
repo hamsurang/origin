40b980ee37df75d665dca0f15a25506f485474fa
minsoo-web <zlemzlem5656@naver.com>
2024-09-15 18:46:01 +0900

    미리 해두는 작업

--- apps/home/app/(profile-readme)/@readme/people/page.tsx
+++ apps/home/app/(profile-readme)/@readme/people/page.tsx
@@ -2,12 +2,25 @@ import { Readme } from '@/_shared'
 import { promises as fs } from 'node:fs'
 import path from 'node:path'
 
+const README_FROM_GITHUB_USERS = [{ username: 'sonsurim', branch: 'master' }] as const
+
 export default async function Page({
   searchParams,
 }: {
   searchParams: { username: string }
 }) {
   const { username } = searchParams
+
+  const githubUser = README_FROM_GITHUB_USERS.find((user) => user.username === username)
+
+  if (githubUser) {
+    const readme = await fetch(
+      `https://raw.githubusercontent.com/${username}/${username}/${githubUser.branch}/README.md`,
+    ).then((res) => res.text())
+
+    return <Readme text={readme} />
+  }
+
   const filePath = path.join(
     process.cwd(),
     `./app/(profile-readme)/@readme/_shared/content/${username}.mdx`,
