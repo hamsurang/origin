88d4da540ee7118c0325c23590e6b47e0e5b589e
minsoo-web <zlemzlem5656@naver.com>
2024-09-15 22:05:24 +0900

    hotfix: detail 태그의 arrow marker가 safari에서 중복해서 보이는 문제 해결

--- apps/wiki/app/page.tsx
+++ apps/wiki/app/page.tsx
@@ -1,4 +1,5 @@
 import { DEFAULT_NAV_ITEM_ID, Docs } from '@/_shared'
+import './reset.css'
 
 export default function Page({ searchParams }: { searchParams: Record<'id', string> }) {
   return <Docs id={searchParams.id || DEFAULT_NAV_ITEM_ID} />
