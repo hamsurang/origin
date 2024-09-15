058fa5e1ed5ef51e6e21ba05fe115205d340c833
sonsurim <surim014@naver.com>
2024-09-15 18:52:55 +0900

    mdx styling components 공통 패키지로 분리

--- apps/wiki/app/_shared/components/Readme/Readme.tsx
+++ apps/wiki/app/_shared/components/Readme/Readme.tsx
@@ -1,6 +1,5 @@
-import { cn } from '@hamsurang/ui'
+import { MDXStylingComponents, cn } from '@hamsurang/ui'
 import { MDXRemote } from 'next-mdx-remote/rsc'
-import { MDXStylingComponents } from './MDX.styles'
 
 export const Readme = ({ text, className }: { text: string; className?: string }) => (
   <div className={cn('border-slate-200 border-2 rounded-lg p-[24px]', className)}>
