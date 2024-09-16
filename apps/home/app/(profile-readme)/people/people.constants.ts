f7fe5f9d8817fe7d21388d980a1b07e656a6c505
Chung-il <jungchungil7@gmail.com>
2024-09-16 12:36:14 +0900

    docs: 퉁이리 MBTI 버그 수정 (#43)
    * docs: 퉁이리 MBTI 버그 수정
    
    * chore: ESFP description 줄바꿈

--- apps/home/app/(profile-readme)/people/people.constants.ts
+++ apps/home/app/(profile-readme)/people/people.constants.ts
@@ -26,10 +26,10 @@ export const PEOPLE_MBTI_INFO_MAP = {
     생활_양식: { value: 'P', percent: 100 },
   },
   tooooo1: {
-    에너지_방향: { value: 'E', percent: 100 },
-    인식_기능: { value: 'N', percent: 100 },
-    판단_기능: { value: 'T', percent: 100 },
-    생활_양식: { value: 'P', percent: 100 },
+    에너지_방향: { value: 'E', percent: 80 },
+    인식_기능: { value: 'S', percent: 55 },
+    판단_기능: { value: 'F', percent: 51 },
+    생활_양식: { value: 'P', percent: 53 },
   },
   yejineee: {
     에너지_방향: { value: 'I', percent: 100 },
